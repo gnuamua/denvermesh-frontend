2eca0d54e543799827a76b1092793dfc26fd3436
gnuamua <andrew@hoa-colors.com>
2024-08-13 16:55:54 -0600

    fixed iframe for focoMesh map. Added meshmapBlock

--- frontend/src/addons/volto-frontend/src/components/Blocks/meshmapBlock/view.jsx
+++ frontend/src/addons/volto-frontend/src/components/Blocks/meshmapBlock/view.jsx
@@ -4,7 +4,6 @@ import { Segment, SegmentGroup, Container , Image, Button, Progress, Embed
  } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
-import bgPNG from './web-logo-square1.png';
 
 
 
