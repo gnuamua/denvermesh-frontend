37af7041f503b59e1772e116bdd1d6386e50b761
gnuamua <andrew@hoa-colors.com>
2024-11-04 01:16:18 -0700

    Update to meeting block code. Added 'voting is in progress' -for foco mesh polls.

--- frontend/src/addons/volto-frontend/src/components/Blocks/meetingBlock/view.jsx
+++ frontend/src/addons/volto-frontend/src/components/Blocks/meetingBlock/view.jsx
@@ -18,10 +18,7 @@ const src = './web-logo-square1.png';
                     <span style={{paddingLeft: '10px'}}>Date: <span style={{fontSize: '16px', paddingLeft: '8px'}}>&nbsp;&nbsp;&nbsp;TBA</span>  </span>
                     <br/>
                     <span style={{paddingLeft: '10px'}}>Time
-                        <span style={{fontSize: '16px', paddingLeft: '8px'}}>&nbsp;&nbsp;&nbsp;&nbsp;TBA</span>
-
-                        <span style={{fontSize: '16px', paddingLeft: '8px'}}>&nbsp;&nbsp;&nbsp;&nbsp;Voting in Progress</span> 
-
+                    <span style={{fontSize: '16px', paddingLeft: '8px'}}>&nbsp;&nbsp;&nbsp;&nbsp;Voting in Progress</span> 
                     </span>
                     <br/>
                     <span style={{paddingLeft: '10px'}}>Where: <span style={{fontSize: '16px', paddingLeft: '6px'}}>&nbsp;<a href='https://denhac.org'> Denhac</a></span>  </span>
