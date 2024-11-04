92a29a1bbaef79e5a189219d6fff43f14048bbab
gnuamua <andrew@hoa-colors.com>
2024-11-04 01:10:58 -0700

    updated donateBlock for donations recieved Nov 2024.

--- frontend/src/addons/volto-frontend/src/components/Blocks/donateBlock/view.jsx
+++ frontend/src/addons/volto-frontend/src/components/Blocks/donateBlock/view.jsx
@@ -16,10 +16,11 @@ import { Link } from 'react-router-dom';
                     <Button inverted color='olive' style={{marginLeft: '32%'}} as='a' href="https://www.paypal.com/donate/?business=L3PBYDKZA2J4G&no_recurring=0&item_name=DenverMesh.org+-Not+Tax+Deductible+until+we+register+as+a+non+profit.&currency_code=USD">Donate</Button>  
                     <br/>
                     <br/>
-                    <Progress percent={2} indicating />
-                    <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px'}}>Currently $0 Donated</h4>
+                    <Progress percent={100} indicating />
+                    <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px', marginRight: '5px'}}>We recieved very generous donations this month from our members.</h4>
+                    <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px'}}>Thank You!!</h4>
                     <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px'}}>Goal is $24 for November 2024</h4>
-                    <span style={{textAlign: 'center', color: 'orange'}}>We are in the process of putting together a Donation Drive to gather the funding necessary to file as a Non-Profit Organization.</span>
+                    <h5 style={{fontWeight: 'bold' , paddingLeft: '5px', textAlign: 'center', marginTop: '0px', marginRight: '5px', color: 'orange'}}>We are in the process of putting together a Donation Drive to gather the funding necessary to file as a Non-Profit Organization.</h5>
                 </Container>
           </div>
         </>
