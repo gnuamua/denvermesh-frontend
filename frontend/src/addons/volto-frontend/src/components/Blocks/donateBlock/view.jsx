5aee4c8d09bc86a8c6bd7526fc8bc5aa3ba1fede
gnuamua <andrew@hoa-colors.com>
2025-01-01 22:12:12 -0700

    Updated donation page info for Jan 2025.

--- frontend/src/addons/volto-frontend/src/components/Blocks/donateBlock/view.jsx
+++ frontend/src/addons/volto-frontend/src/components/Blocks/donateBlock/view.jsx
@@ -16,10 +16,9 @@ import { Link } from 'react-router-dom';
                     <Button inverted color='olive' style={{marginLeft: '32%'}} as='a' href="https://www.paypal.com/donate/?business=L3PBYDKZA2J4G&no_recurring=0&item_name=DenverMesh.org+-Not+Tax+Deductible+until+we+register+as+a+non+profit.&currency_code=USD">Donate</Button>  
                     <br/>
                     <br/>
-                    <Progress percent={100} indicating />
-                    <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px', marginRight: '5px'}}>We recieved very generous donations this month from our members.</h4>
+                    <Progress percent={10} indicating />
                     <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px'}}>Thank You!!</h4>
-                    <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px'}}>Goal is $24 for November 2024</h4>
+                    <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px'}}>Goal is $24 for January 2025</h4>
                     <h5 style={{fontWeight: 'bold' , paddingLeft: '5px', textAlign: 'center', marginTop: '0px', marginRight: '5px', color: 'orange'}}>We are in the process of putting together a Donation Drive to gather the funding necessary to file as a Non-Profit Organization.</h5>
                 </Container>
           </div>
