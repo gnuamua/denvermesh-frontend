8cf52febc5005bd270c2d54853b5ee70a63fdd2f
gnuamua <andrew@hoa-colors.com>
2025-01-01 22:35:54 -0700

    Ammended the DonationBlock to include unordered list of donation fund uses.

--- frontend/src/addons/volto-frontend/src/components/Blocks/donateBlock/view.jsx
+++ frontend/src/addons/volto-frontend/src/components/Blocks/donateBlock/view.jsx
@@ -19,7 +19,15 @@ import { Link } from 'react-router-dom';
                     <Progress percent={10} indicating />
                     <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px'}}>Thank You!!</h4>
                     <h4 style={{fontWeight: 'bold' , paddingLeft: '15px', textAlign: 'center', color: '#00ec97', marginTop: '0px'}}>Goal is $24 for January 2025</h4>
-                    <h5 style={{fontWeight: 'bold' , paddingLeft: '5px', textAlign: 'center', marginTop: '0px', marginRight: '5px', color: 'orange'}}>We are in the process of putting together a Donation Drive to gather the funding necessary to file as a Non-Profit Organization.</h5>
+                    <h5 style={{fontWeight: 'bold' , paddingLeft: '5px', textAlign: 'center', marginTop: '0px', marginRight: '5px', color: 'orange'}}>Donations are used for the following:</h5>
+                    <ul style={{fontWeight: 'bold' , textAlign: 'left', fontSize: '12px', marginTop: '0px',  color: 'orange'}}>
+                        <li>Linode Server costs</li>
+                        <li>Mattermost Instance Hosting</li>
+                        <li>Email Server Hosting (Free member @denvermesh.org email addresses available)</li>
+                        <li>Stickers & Other Materials</li>
+                        <li>Domain Name Registration</li>
+                    </ul>
+                    <h4 style={{fontWeight: 'bold' , textAlign: 'left', fontSize: '14px', marginTop: '0px',  color: 'orange'}}>We are looking to Register as a Non-profit Organization in the Near Future</h4>
                 </Container>
           </div>
         </>
