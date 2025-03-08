d11a046021585a1faed9bd77df5cf7316354eece
gnuamua <andrew@hoa-colors.com>
2025-03-08 01:52:21 -0700

    Updated index.js in Blocks dir.

--- frontend/src/addons/volto-frontend/src/components/index.js
+++ frontend/src/addons/volto-frontend/src/components/index.js
@@ -9,7 +9,7 @@
  * };
  */
 
-import MeetingViewBlock from './Blocks/meetingBlock/view';
+
 import MattermostViewBlock from './Blocks/mattermostBlock/view';
 import DonateViewBlock from './Blocks/donateBlock/view';
 import FocomapViewBlock from './Blocks/focoMapBlock/view';
@@ -17,17 +17,12 @@ import MeshmapViewBlock from './Blocks/meshmapBlock/view';
 import MeshmapEditBlock from './Blocks/meshmapBlock/edit';
 import NetViewBlock from './Blocks/netblock/view';
 import NetEditBlock from './Blocks/netblock/edit';
-import MeetingEditBlock from './Blocks/meetingBlock/edit';
 import MattermostEditBlock from './Blocks/mattermostBlock/edit';
 import DonateEditBlock from './Blocks/donateBlock/edit';
 import FocomapEditBlock from './Blocks/focoMapBlock/edit';
-import MeetingListEditWidget from './Blocks/meetingBlock/MeetingListEditWidget';
 
-export { MeetingViewBlock };
 export { MattermostViewBlock };
-export { MeetingEditBlock };
 export { MattermostEditBlock };
-export { MeetingListEditWidget };
 export { DonateViewBlock };
 export { DonateEditBlock };
 export { FocomapEditBlock };
